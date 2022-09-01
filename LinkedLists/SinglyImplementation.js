9539ee44c90492d2db9c26f25a3d36862062176f
Priyanka Daryani <11776756+priyankadaryani@users.noreply.github.com>
2022-09-01 19:06:15 -0400

    Update SinglyImplementation.js
    add reverse LL method

--- LinkedLists/SinglyImplementation.js
+++ LinkedLists/SinglyImplementation.js
@@ -72,6 +72,26 @@ class LinkedList {
     this.length--;
     return this.printList();
   }
+  reverse() {
+      if (!this.head.next) {
+        return this.head;
+      }
+      let first = this.head;
+      this.tail = this.head;
+      let second = first.next;
+  
+      while(second) {
+        const temp = second.next;
+        second.next = first;
+        first = second;
+        second = temp;
+      }
+  
+      this.head.next = null;
+      this.head = first;
+      return this.printList();
+    }
+  
 }
 
 let myLinkedList = new LinkedList(10);
